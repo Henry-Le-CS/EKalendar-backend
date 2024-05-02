b7654061b363f0f7f3cc9bdf47c4c70e1e8e61eb
Henry Le <lehieu2077@gmail.com>
2024-05-02 19:58:59 +0700

    feat: add parse date helper

--- cmd/common/helper.go
+++ cmd/common/helper.go
@@ -3,6 +3,7 @@ package common
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
 func GenerateResponse(data interface{}, err string) []byte {
@@ -20,4 +21,15 @@ func RaiseBadRequest(w http.ResponseWriter, err string) {
 	res := GenerateResponse(nil, err)
 	w.WriteHeader(http.StatusBadRequest)
 	w.Write(res)
+}
+
+func ParseDate(date string, layout string) (time.Time, error) {
+	// Issue: https://stackoverflow.com/questions/43456851/how-to-format-todays-date-in-go-as-dd-mm-yyyy
+	if layout == "dd/MM/yyyy" {
+		layout = "02/01/2006"
+	} else if layout == "MM/dd/yyyy" {
+		layout = "01/02/2006"
+	}
+
+    return time.Parse(layout, date)
 }
