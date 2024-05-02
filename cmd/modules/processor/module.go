f9e05e283885fe5b10920c70a0c9cb1a4d7c0d5e
Henry Le <lehieu2077@gmail.com>
2024-05-03 01:45:19 +0700

    chore: remove unused codes

--- cmd/modules/processor/module.go
+++ cmd/modules/processor/module.go
@@ -3,7 +3,7 @@ package processor
 import "github.com/Henry-Le-CS/gost"
 
 func ProcessorModule() *gost.Module {
-	controllers := []gost.IController{ProcessorControllers()}
+	controllers := []gost.IController{}
 
 	module := gost.DeclareModule(gost.RegisterModuleDto{
 		Name: "Processor module",
